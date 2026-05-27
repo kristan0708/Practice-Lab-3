bdc41ac6536cfa67acd3193e95e3fe0f104f7540
kristan0708 <adriosulakza.25@ichat.sp.edu.sg>
2026-05-27 17:31:12 +0800

    I added the testing for both the age range and department functions

--- test_employee_info.py
+++ test_employee_info.py
@@ -1,7 +1,13 @@
 import employee_info as em
 
 
-#def test_employees_by_age_range():
+def test_employees_by_age_range():
+    expected_result = [{"name": "John", "age": 30, "department": "Sales", "salary": 50000},
+                       {"name": "Chloe",  "age": 35, "department": "Engineering", "salary": 70000},
+                       {"name": "Mike", "age": 32, "department": "Engineering", "salary": 65000}]
+    result = em.get_employees_by_age_range(25,38)
+
+    assert(expected_result == result)
 
 
 def test_calculate_average_salary():
@@ -10,4 +16,8 @@ def test_calculate_average_salary():
 
     assert(expected_result == result)
 
-#def test_get_employees_by_dept():
+def test_get_employees_by_dept():
+    expected_result = ["John","Peter"]
+    result = em.get_employees_by_dept("Sales")
+
+    assert(expected_result == result)
